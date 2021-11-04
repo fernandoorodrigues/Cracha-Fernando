f96cd5969afa274b30bf9bcd98aee6225175318a
fernando de oliveira rodrigues <fernandoo.rodrigues9@gmail.com>
2021-11-04 14:04:35 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'fernandoorodrigues',
+  youtube: 'qnbYmfcy3oKrqKn6miryEA',
+  facebook: 'fernandoo.rodrigues9',
+  instagram: 'fernandoo.o.r',
+  twitter: '/'
+};
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class');
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`;
+  }
+}
+changeSocialMediaLinks();
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name;
+      userBio.textContent = data.bio;
+      userLink.href = data.html_url;
+      userPhoto.src = data.avatar_url;
+      userLogin.textContent = data.login;
+    });
+}
+
+getGitHubProfileInfos();
